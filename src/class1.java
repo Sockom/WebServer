ed8680ffda73f4b4dade2012f5735e3979e25db6
Sockom <itsallhate@hotmail.com>
2021-04-21 15:51:26 +0200

    2

--- src/class1.java
+++ src/class1.java
@@ -1,2 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class class1 {
+
+    public class1() throws SQLException {
+        DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
+        String dbURL = "jdbc:sqlserver://DESKTOP-P2FRPBU\\user=ItsAl:1433";
+        Connection connection = DriverManager.getConnection(dbURL);
+        if (connection != null){
+            System.out.println("Connected");
+        }
+        connection.prepareStatement("insert into DimEjer VALUES Kasper");
+        connection.close();
+
+    }
 }
