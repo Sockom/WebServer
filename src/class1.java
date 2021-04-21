947864691d4c1057bf7b931eb25b426977cd58cd
Sockom <itsallhate@hotmail.com>
2021-04-21 16:23:24 +0200

    It works yay husk ''

--- src/class1.java
+++ src/class1.java
@@ -1,17 +1,19 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class class1 {
 
     public class1() throws SQLException {
         DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
-        String dbURL = "jdbc:sqlserver://DESKTOP-P2FRPBU\\user=ItsAl:1433";
+        String dbURL = "jdbc:sqlserver://DESKTOP-P2FRPBU\\MSSQLSERVER;user=Host;password=1234";
         Connection connection = DriverManager.getConnection(dbURL);
         if (connection != null){
             System.out.println("Connected");
         }
-        connection.prepareStatement("insert into DimEjer VALUES Kasper");
+
+        connection.prepareStatement("insert into stage.DimEjer " + "VALUES ('Kaspers')").execute();
         connection.close();
 
     }
