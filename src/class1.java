7d8d05613e2e00bf3afc35a58b9bb4189d9afe6e
Sockom <itsallhate@hotmail.com>
2021-04-22 12:55:41 +0200

    COMPUTER ID ADDED

--- src/class1.java
+++ src/class1.java
@@ -1,13 +1,13 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class class1 {
 
     public class1() throws SQLException {
         DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
-        String dbURL = "jdbc:sqlserver://DESKTOP-SMPCJNQ\\MSSQLSERVER;user=Host;password=1234;database=GrowBroDWH";//K:SMPCJNQ Mk:P2FRPBU
+        String dbURL = "jdbc:sqlserver://DESKTOP-SMPCJNQ" +
+                "\\MSSQLSERVER;user=Host;password=1234;database=GrowBroDWH";//K:SMPCJNQ Mk:P2FRPBU KH:ASU6SHH MT:HFHHMQP
         Connection connection = DriverManager.getConnection(dbURL);
         if (connection != null){
             System.out.println("Connected");
