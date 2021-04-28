765c7ef280ddf877bf1357d9f7f8616e8cf7b31b
Sockom <itsallhate@hotmail.com>
2021-04-28 16:29:46 +0200

    GetUserID + getPlanteID + insertSensorDataToStage

--- src/SensorData.java
+++ src/SensorData.java
@@ -6,27 +6,53 @@ import java.util.Random;
 
 public class SensorData {
 
-    byte[] Data;
-    Timestamp timestamp;
-    Gson gson;
+    private byte[] Data;
+    private Gson gson;
+    private int[] temperatur = new int[15];
+    private int[] CO2 = new int[15];
+    private int[] luftfugtighed = new int[15];
+    private Timestamp[] timestamps = new Timestamp[15];
+    private int drivhusID;
+    private int numberOf;
+
 
     public SensorData() {
         gson = new Gson();
         Random tal = new Random();
-        int[] temperatur = new int[15];
-        int[] CO2 = new int[15];
-        int[] luftfugtighed = new int[15];
-        Timestamp[] timestamps = new Timestamp[15];
+        drivhusID = 1;
+        numberOf = 15;
         for (int i = 0 ; i < 15 ; i++) {
             CO2[i] = tal.nextInt(1600);
             temperatur[i] = tal.nextInt(100);
             luftfugtighed[i] = tal.nextInt(100);
             timestamps[i] = new Timestamp(new Date().getTime()-60000 * i);
         }
-        ByteArray byteArray = new ByteArray(1, CO2, timestamps, temperatur, luftfugtighed, 15);
+        ByteArray byteArray = new ByteArray(drivhusID, CO2, timestamps, temperatur, luftfugtighed, 15);
 
         System.out.println(gson.toJson(byteArray));
     }
 
+    public int[] getTemperatur() {
+        return temperatur;
+    }
 
+    public int[] getCO2() {
+        return CO2;
+    }
+
+    public int[] getLuftfugtighed() {
+        return luftfugtighed;
+    }
+
+    public Timestamp[] getTimestamps() {
+        return timestamps;
+    }
+
+    public int getDrivhusID() {
+        return drivhusID;
+    }
+
+    public int getNumberOf() {
+        return numberOf;
+    }
 }
